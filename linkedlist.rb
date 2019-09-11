bfeb198fe29eaa8f3bd0751938bfe70d5122a65c
Matthew Flores <flo2312@gmail.com>
2019-09-11 00:44:34 +0000

    Initial commit

---
+++ linkedlist.rb
@@ -0,0 +1,26 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+# stack class
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
